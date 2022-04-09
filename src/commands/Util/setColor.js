11f96a0a35ce700d29a08f438b4c53c6bd3b7a0f
Ravy <ravy@farfrom.earth>
2022-04-09 17:54:48 +0200

    item order?

--- src/commands/Util/setColor.js
+++ src/commands/Util/setColor.js
@@ -21,8 +21,8 @@ module.exports = class extends Command {
 		const colorData = tc(color);
 		if (colorData._format === false) return msg.responder.error('COMMAND_SETCOLOR_INVALIDCOLOR');
 		return role.setColor(colorData.toHex())
-			.catch(() => msg.responder.error('COMMAND_SETCOLOR_NOPERMS'))
-			.then(() => msg.responder.success());
+			.then(() => msg.responder.success())
+			.catch(() => msg.responder.error('COMMAND_SETCOLOR_NOPERMS'));
 	}
 
 };

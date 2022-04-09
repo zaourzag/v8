08e7d770b1e3c2b1fa5cf28e43df5332742db7de
Ravy <ravy@farfrom.earth>
2022-04-09 18:28:25 +0200

    updated bad name regex

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -12,7 +12,7 @@ module.exports = class extends Event {
 		});
 
 		this.bannedMemberNames = [
-			/(mod(erator)?('?s)?|hype\s*squad|developers?)\s+(academy|message|exam)/i,
+			/(mod(erator)?('?s)?|hype\s*squad|developers?)\s+(academy|message|exam|team|events?)/i,
 			/discord\s+hype\s*squad/i,
 			/(hype\s*squad|discord)\s+(events?|academy)/i,
 			/discord\s+(developers?|api|bots?|message)?/i,

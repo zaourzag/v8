61ed83ca6102cb4c1e4e609c4838564281ddae1a
Ravy <ravy@farfrom.earth>
2022-05-08 04:17:20 +0200

    fix: autopublisher not working

--- src/monitors/autopublisher.js
+++ src/monitors/autopublisher.js
@@ -14,7 +14,7 @@ module.exports = class extends Monitor {
 	}
 
 	async run(msg) {
-		if (!msg.guild || !(msg.channel?.type === 'news')) return false;
+		if (!msg.guild || msg.channel?.type !== 'GUILD_NEWS' || !msg.crosspostable) return false;
 
 		const possibles = msg.guild.settings.get('autopublish');
 

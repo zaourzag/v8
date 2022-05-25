a28b1a3a4394a3ac1142ad63ef9680c36a38c347
Ravy <ravy@farfrom.earth>
2022-05-25 17:14:08 +0200

    lower perspective threshold

--- src/commands/Admin/perspectiveTest.js
+++ src/commands/Admin/perspectiveTest.js
@@ -31,7 +31,7 @@ module.exports = class extends Command {
 			.header('user-agent', `${this.client.user.username}/${this.client.config.version}`)
 			.json();
 
-		const content = Object.entries(res.attributeScores).map(([key, score]) => `${ansi}${score.summaryScore.value > 0.9
+		const content = Object.entries(res.attributeScores).map(([key, score]) => `${ansi}${score.summaryScore.value > 0.8
 			? ansi.bold().color('red')
 			: ansi.color('green')
 		} ${key.padEnd(17)} = ${score.summaryScore.value}${ansi.reset()}`).join('\n');

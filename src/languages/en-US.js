256af6587e622da0c109807a80c6796ff51c9ff3
Ravy <ravy@farfrom.earth>
2022-07-11 17:00:01 +0200

    add filtering based on heuristics

--- src/languages/en-US.js
+++ src/languages/en-US.js
@@ -165,6 +165,9 @@ module.exports = class extends Language {
 				'To begin configuring it, just re-run this command and specify a channel.'
 			].join(' '),
 			COMMAND_LEMONBOARD_NOCHANNEL: 'Please configure a channel for your lemonboard first.',
+			COMMAND_HEURISTICS_DESCRIPTION: 'Configure whether detection of common bad patterns should be used to automatically ban users.',
+			COMMAND_HEURISTICS_ENABLED: 'Enabled automatic banning of users.',
+			COMMAND_HEURISTICS_DISABLED: 'Disabled automatic banning of users.',
 
 			// fun commands
 			COMMAND_8BALL_DESCRIPTION: 'Magic 8-Ball, does exactly what the toy does.',
@@ -699,6 +702,7 @@ module.exports = class extends Language {
 			MONITOR_COMMAND_HANDLER_ABORTED: `${success} aborted.`,
 			MONITOR_ANTI_SCAMS: (message) => `Marked as fraudulent by anti-scams module: ${message}`,
 			MONITOR_ANTI_MASSMENTIONS: (threshold) => `Anti-massmention: sent more than ${threshold} mentions in a short timeframe.`,
+			MONITOR_HEURISTICS: 'Marked as very suspicious using raid/scam heuristics. "[p]heuristics disable" to turn off.',
 			// eslint-disable-next-line max-len
 			INHIBITOR_COOLDOWN: (remaining, guildCooldown) => `${guildCooldown ? 'Someone has' : 'You have'} already used this command. You can use this command again in ${remaining}.`,
 			INHIBITOR_DISABLED_GUILD: 'This command has been disabled by an admin in this guild.',
---
+++ src/monitors/heuristics.js
@@ -0,0 +1,33 @@
+const { Monitor } = require('@aero/framework');
+
+module.exports = class extends Monitor {
+
+	constructor(...args) {
+		super(...args, {
+			enabled: true,
+			ignoreBots: true,
+			ignoreSelf: true,
+			ignoreEdits: false,
+			ignoreOthers: false
+		});
+
+        this.circumventionPatternNameRegex = /^[A-Z][a-z]+[A-Z][a-z]+[0-9]+$/;
+        this.circumventionPatternMessageRegex = /^hi$/i;
+	}
+
+	async run(msg) {
+		if (!msg.guild || !msg.guild.settings.get('mod.heuristics') || msg.exempt) return;
+
+		if (this.matchesCircumventionPattern(msg)) {
+            msg.member.ban({ reason: msg.language.get('MONITOR_HEURISTICS'), days: 1 })
+        }
+	}
+
+	matchesCircumventionPattern(msg) {
+		return msg.content
+			&& !msg.member.prevMessageContent
+			&& this.circumventionPatternNameRegex.test(msg.author.username)
+			&& this.circumventionPatternMessageRegex.test(msg.content);
+	}
+
+};

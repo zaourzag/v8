4791940eab326ab28e951832ceaf128b9c271ee3
Willy <jemand771@gmx.net>
2022-10-24 20:03:39 +0200

    fix voice moderation commands (d.js v13)
    discord.js v13 renamed a bunch of id-related attributes from `xxxID` to `xxxId` , e.g. `channelID` -> `channelId`.
    this was mostly addressed in 78945047, but for the voice-related moderation commands, it wasn't.
    funnily enough, it took over half a year for anyone to notice they're broken.
    
    see: https://discordjs.guide/additional-info/changes-in-v13.html#naming-conventions

--- src/commands/Moderation/Voice/vcundeafen.js
+++ src/commands/Moderation/Voice/vcundeafen.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 	}
 	async run(msg, [user, reason]) {
 		if (!msg.member.permissions.has('DEAFEN_MEMBERS')) return msg.responder.error('COMMAND_VOICEUNDEAFEN_NOPERMS');
-		if (!user.voice.channelID) return msg.responder.error('COMMAND_VOICEUNDEAFEN_NOVOICE');
+		if (!user.voice.channelId) return msg.responder.error('COMMAND_VOICEUNDEAFEN_NOVOICE');
 		if (!user.voice.serverDeaf) return msg.responder.error('COMMAND_VOICEDEAFEN_ALREADY_UNDEAFENED');
 		await user.voice.setDeaf(false, reason || msg.language.get('COMMAND_VOICEUNDEAFEN_NOREASON'));
 		return msg.responder.success();

670b5cfac018a29b634269454a50f264a0b4981c
Hazel <git@ravy.org>
2023-10-18 14:11:25 +0100

    so much more logging

--- src/index.js
+++ src/index.js
@@ -98,6 +98,26 @@ async function main() {
 		aggregator.login(worker);
 	});
 
+	cluster.on('debug', (msg) => {
+		logger.debug(msg);
+	});
+
+	cluster.on('shardReady', (id) => {
+		logger.log(`[Sharder] Shard ${id} ready.`);
+	});
+
+	cluster.on('shardDisconnect', (event, id) => {
+		logger.log(`[Sharder] Shard ${id} disconnected: ${event.reason}`);
+	});
+
+	cluster.on('shardResume', (_replayed, id) => {
+		logger.log(`[Sharder] Shard ${id} resumed.`);
+	});
+
+	cluster.on('shardReconnect', (id) => {
+		logger.log(`[Sharder] Shard ${id} reconnected.`);
+	});
+
 	const sharder = new ShardingManager(join(__dirname, 'launch'), {
 		client: Aero,
 		ipcSocket,
